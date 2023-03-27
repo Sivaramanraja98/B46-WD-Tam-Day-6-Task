8721d11b0bf0058157f4ae1a6e24597f5bef9000
Sivaramanraja98 <127581183+Sivaramanraja98@users.noreply.github.com>
2023-03-27 21:57:54 +0530

    Update 2.circle.js

--- 2.circle.js
+++ 2.circle.js
@@ -1,1 +1,37 @@
+class Circle{
+    constructor(radius,color)
+    {
+        this.radius= radius;
+        this.color = color;
+    }
+    getradius()
+    {
+        return this.radius;
+    }
+    setradius()
+    {
+        return this.getradius();
+    }
+    getcolour()
+    {
+        return this.color;
+    }
+    setcolour()
+    {
+        return this.getcolour();
+    }
+    getarea()
+    {
+        return (Math.PI*(this.radius*this.radius)).toFixed(2);
+    }
+    getcircumference()
+    {
+        return (Math.PI*(2*this.radius)).toFixed(2);
+    }
+}
 
+let calc = new Circle(9,'red')
+console.log(calc.setcolour());
+console.log(calc.setradius());
+console.log(calc.getarea());
+console.log(calc.getcircumference());
